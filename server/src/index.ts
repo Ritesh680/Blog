d24ba6e2c26c79a80de537c0f27b59771da9adfd
Ritesh Poudel <ritesh.poudel@kotuko.it>
2024-06-01 17:18:03 +0545

    update cors origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,8 +8,9 @@ const app: Express = express();
 const port = config.port || 3000;
 
 const corsOptions = {
-	origin: "*",
+	origin: ["https://blog-ritesh.netlify.app", "http://localhost:5173"],
 	methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+	credentials: true,
 };
 app.use(express.json());
 app.use(cors(corsOptions));
