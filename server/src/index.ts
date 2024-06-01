88cfd52b2fc5046281e932db18b42d1b12827873
Ritesh Poudel <ritesh.poudel@kotuko.it>
2024-06-01 20:51:31 +0545

    added netlify.toml file and splitted github actions

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,5 @@
 import express, { Express } from "express";
 import db from "./db/connection";
-import cacheControl from "express-cache-controller";
 
 // your routes here
 import cors, { CorsOptions } from "cors";
@@ -9,21 +8,10 @@ const config = Config(process.env.NODE_ENV);
 
 const app: Express = express();
 const port = config.port || 3000;
-app.use(
-	cacheControl({
-		maxAge: 0,
-	})
-);
 
 const corsOptions: CorsOptions = {
-	origin: ["https://blog-ritesh.netlify.app", "http://localhost:5173", "*"],
+	origin: "*",
 	methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-	allowedHeaders: ["Content-Type", "Authorization"],
-	credentials: true,
-	maxAge: 0,
-	preflightContinue: true,
-	optionsSuccessStatus: 204,
-	exposedHeaders: ["Authorization"],
 };
 app.use(express.json());
 app.use(cors(corsOptions));
